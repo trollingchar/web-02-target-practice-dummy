2d8a7922d34e494bb5cd89cd9d6ac1a76acc2a9b
trollingchar <test>
2025-07-02 22:17:44 +0300

    ...

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 fetch('https://trollingchar.github.io/web-02-target-practice-dummy/images/cat.png').then(response => {
-    ok = true;
+    ok = response.ok;
     console.log(response);
 }).catch(error => console.error(error));
 
 
 // validate
 // ok = false;
-let el = document.getElementById("result");
-el.innerText = ok ? "Validation successful" : "Validation failed";
-el.classList.add(ok ? 'text-lime-400' : 'text-red-500');
+setTimeout(() => {
+    let el = document.getElementById("result");
+    el.innerText = ok ? "Validation successful" : "Validation failed";
+    el.classList.add(ok ? 'text-lime-400' : 'text-red-500');
+}, 1000);
