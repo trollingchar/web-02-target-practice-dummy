4490be85e9f9c4247d89c2c5d2f0f491bc70dd22
Родин Д.А <test@test.test>
2025-07-03 10:34:44 +0300

    ...

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-fetch('https://trollingchar.github.io/web-02-target-practice-dummy/images/cat.png').then(response => {
-    ok = response.ok;
-    console.log(response);
-}).catch(error => console.error(error));
+window.onload = function() {
+    fetch('https://trollingchar.github.io/web-02-target-practice-dummy/images/cat.png').then(response => {
+        ok = response.ok;
+        console.log(response);
+    }).catch(error => console.error(error));
 
 
-// validate
-setTimeout(() => {
-    ok = false;
-    let el = document.getElementById("result");
-    el.innerText = ok ? "Validation successful" : "Validation failed";
-    el.classList.add(ok ? 'text-lime-400' : 'text-red-500');
-}, 1000);
+    // validate
+    setTimeout(() => {
+        ok = false;
+        let el = document.getElementById("result");
+        el.innerText = ok ? "Validation successful" : "Validation failed";
+        el.classList.add(ok ? 'text-lime-400' : 'text-red-500');
+    }, 1000);
+}
